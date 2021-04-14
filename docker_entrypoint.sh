56707f61491da4611a8d227f60b2c4af36971b4f
Keagan McClelland <keagan.mcclelland@gmail.com>
2021-04-14 16:57:25 -0600

    fixes public/shared backup

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,8 +4,8 @@ export PUBLIC_URL=$TOR_ADDRESS:3300
 export LND_IP=$(yq e '.lightning-config.lnd' /relay/.lnd/start9/config.yaml)
 export PASSWORD=$(yq e '.password' /relay/.lnd/start9/config.yaml)
 
-echo "/relay/.lnd/start9/public" > /relay/.lnd/.backupignore
-echo "/relay/.lnd/start9/shared" >> /relay/.lnd/.backupignore
+echo start9/public > .backupignore
+echo start9/shared >> .backupignore
 jq '.production.macaroon_location = "/relay/.lnd/start9/public/lnd/admin.macaroon"' /relay/dist/config/app.json > /relay/dist/config/app.json.tmp && mv /relay/dist/config/app.json.tmp /relay/dist/config/app.json
 jq '.production.tls_location = "/relay/.lnd/start9/public/lnd/tls.cert"' /relay/dist/config/app.json > /relay/dist/config/app.json.tmp && mv /relay/dist/config/app.json.tmp /relay/dist/config/app.json
 jq '.production.connection_string_path = "/relay/.lnd/connection_string.txt"' /relay/dist/config/app.json > /relay/dist/config/app.json.tmp && mv /relay/dist/config/app.json.tmp /relay/dist/config/app.json
