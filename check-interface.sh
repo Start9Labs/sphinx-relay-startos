26e7e3aa99ec730622b1a5b45ba7841b0a8d226d
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-02-25 17:53:33 -0700

    update health check copy

--- check-interface.sh
+++ check-interface.sh
@@ -7,7 +7,7 @@ else
     curl --silent --fail sphinx-relay.embassy:3300/app &>/dev/null
     exit_code=$?
     if test "$exit_code" != 0; then
-        echo "Web interface is unreachable" >&2
+        echo "Cannot connect to API" >&2
         exit 1
     fi
 fi
